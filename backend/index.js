fcb46f25c0630f0d1d6a35a4acb37d95b1991908
enyoukai <an.josephZero@gmail.com>
2022-09-27 21:04:26 +0000

    basic api

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+let clubs = {
+  1: {
+    id: '1',
+    username: 'programming',
+  },
+  2: {
+    id: '2',
+    username: 'robo',
+  },
+};
+
+app.get('/clubs', (req, res) => {
+  return res.send(Object.values(clubs));
+})
+
+app.get('/clubs/:clubId', (req, res) => {
+  return res.send(clubs[req.params.clubId]);
+})
+
+app.post('/clubs', (req, res) => {
+  return res.send('post request');
+})
+
+app.put('/clubs/:clubId', (req, res) => {
+  return res.send(`put request on ${req.params.clubId} id`);
+})
+
+app.delete('/clubs/:clubId', (req, res) => {
+  return res.send(`delete request ${req.params.clubId} id`);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>list of clubs</p>
+      <ul>
+      <p>{fetch("/clubs").then(res => res.json()).then(data => console.log(data))}</p>
+      </ul>
+      <input placeholder="club name"></input>
+      <button>add club</button>
     </div>
   );
 }
