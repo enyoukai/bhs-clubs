c9cafec701e1927aec250ebeaf7bd5d0ef82bd19
enyoukai <an.josephZero@gmail.com>
2022-10-18 16:06:25 -0700

    removed random import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import SignIn from './routes/SignIn'
 import Register from './routes/Register'
 import NewClub from './routes/NewClub'
 import Account from './routes/Account'
-import { SDK_VERSION } from 'firebase/app';
 
 function App() {
   return (
