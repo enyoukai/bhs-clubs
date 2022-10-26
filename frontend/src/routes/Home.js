5fbfbdc5da8cb5d9a986a764f4fb56a34421faa1
enyoukai <an.josephZero@gmail.com>
2022-10-26 19:40:06 +0000

    recat-css-modules but idk not working

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
-import './Home.module.css';
+import './Home.css';
 import API from '../api/API.js';
+import CSSModules from 'react-css-modules';
 
 function Home() {
   const [clubs, setClubs] = useState([]);
