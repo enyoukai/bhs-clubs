c21ad033a6d643323857b2a2fc7f6b3adfc11e73
enyoukai <an.josephZero@gmail.com>
2022-11-01 19:32:30 -0700

    modify club redirect

--- frontend/src/routes/ModifyClub.js
+++ frontend/src/routes/ModifyClub.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import API from '../api/API';
 
 import { useAuth } from '../contexts/AuthContext'
 
+import Loading from "../components/Loading";
+
 export default function ModifyClub() {
-    const id = useParams().id;
+    const clubId = useParams().id;
     const [club, setClub] = useState({});
 	const [description, setDescription] = useState('');
 	const [location, setLocation] = useState('');
@@ -13,12 +15,18 @@ export default function ModifyClub() {
 	const [time, setTime] = useState('');
 	const [advisor, setAdvisor] = useState('');
 
+    const [loading, setLoading] = useState(true);
+
     const {user} = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function getClub()
         {
-            const club = await API.getClubById(id);
+            const club = await API.getClubById(clubId);
+            if (club.uid != user.uid) navigate('/');
+
+            setLoading(false);
 
             setClub(club);
             setDescription(club.description);
@@ -32,25 +40,29 @@ export default function ModifyClub() {
 
     async function submitChange()
     {
-        await API.putClub(id, club.name, description, location, date, time, advisor, await user.getIdToken());
+        await API.putClub(clubId, club.name, description, location, date, time, advisor, await user.getIdToken());
     }
 
     return (
-        <div className="modify">
-            <header>Modifying {club.name}</header>
-            <div className="modify__form">
-                <div>Description</div>
-                <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
-                <div>Location</div>
-                <input value={location} onChange={e => setLocation(e.target.value)}></input>
-                <div >Day</div>
-                <input value={date} onChange={e => setDate(e.target.value)}></input>
-                <div>Time</div>
-                <input value={time} onChange={e => setTime(e.target.value)}></input>
-                <div>Advisor</div>
-                <input value={advisor} onChange={e => setAdvisor(e.target.value)}></input>
-                <button onClick={submitChange}>submit</button>
-            </div>
-        </div>
+        <>
+            {loading ? <Loading/> :         
+                <div className="modify">
+                <header>Modifying {club.name}</header>
+                <div className="modify__form">
+                    <div>Description</div>
+                    <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
+                    <div>Location</div>
+                    <input value={location} onChange={e => setLocation(e.target.value)}></input>
+                    <div >Day</div>
+                    <input value={date} onChange={e => setDate(e.target.value)}></input>
+                    <div>Time</div>
+                    <input value={time} onChange={e => setTime(e.target.value)}></input>
+                    <div>Advisor</div>
+                    <input value={advisor} onChange={e => setAdvisor(e.target.value)}></input>
+                    <button onClick={submitChange}>submit</button>
+                </div>
+            </div>}
+        </>
+
     )
 }
