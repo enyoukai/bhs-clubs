567f8732360a7fe8d9e9352d8a02c03bcb6dc2f5
joseph an <52297896+enyoukai@users.noreply.github.com>
2022-11-23 22:14:36 +0000

    calendar fetching

--- frontend/src/routes/Calendar.js
+++ frontend/src/routes/Calendar.js
@@ -1,9 +1,8 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 export default function Calendar()
 {
-	var date = new Date();
-	console.log(date.getDate());
-	console.log(date.getDay());
-	console.log(new Date(date.getFullYear(), date.getMonth(), 1));
 	return (
 		<div className="p-10 text-center">
 			<h1 className="text-4xl">Calendar</h1>
@@ -15,7 +14,7 @@ export default function Calendar()
 function CalendarUI()
 {
 	return (
-		<table className="mt-10 py-10 border w-full mb-0">
+		<table className="mt-10 py-10 border w-full">
 			<thead>
 				<tr>
 					<th>Sunday</th>
@@ -29,28 +28,62 @@ function CalendarUI()
 			</thead>
 			<tbody>
 				<tr>
-					<DayColumn clubs={['a', 'b']}/>
-					<DayColumn clubs={['a', 'b']}/>
-					<DayColumn clubs={['a', 'b']}/>
-					<DayColumn clubs={['a', 'b']}/>
-					<DayColumn clubs={['a', 'b']}/>
+					<ClubColumns/>
 				</tr>
 			</tbody>
 		</table>
 	)
 }
 
+function ClubColumns() {
+	const [dayMap, setDayMap] = useState();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function fetch() {
+			const res = await axios.get('/clubs');
+			const clubs = res.data;
+
+			const dayMapNext = Array.apply(null, Array(7)).map(() => []);
+
+			clubs.forEach((club) => {
+				dayMapNext[1].push(club);
+			});
+
+			setDayMap(dayMapNext);
+			setLoading(false);
+		}
+		fetch();
+	}, []);
+
+	return (
+		<>
+		{
+			!loading && dayMap.map((dayCol, idx) => <DayColumn key={idx} clubs={dayCol}/>)
+		}
+			
+		</>
+	)
+}
+
 function DayColumn(props)
 {
-	console.log(props.clubs);
 	return (
-		<td>
+		<td >
 			<ul>
 				{
-					props.clubs.map((club) => <li>{club}</li>)
+					props.clubs.map((club) => <ClubTime key={club.id} club={club}/>)
 				}	
 			</ul>
 		</td>
 	)
+}
 
+function ClubTime(props) {
+	return (
+		<li className='border'>
+			<div>{props.club.time}</div>
+			<div>{props.club.name}</div>
+		</li>
+	)
 }
