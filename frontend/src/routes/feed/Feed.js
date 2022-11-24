7b0846e854e29d1fe4b162ef9c39ccb281fe455b
joseph an <52297896+enyoukai@users.noreply.github.com>
2022-11-24 01:12:51 +0000

    centered feed

--- frontend/src/routes/feed/Feed.js
+++ frontend/src/routes/feed/Feed.js
@@ -13,7 +13,7 @@ export default function Feed()
 	return (
 		<div className="pt-10 px-10">
 			<div className="text-center text-4xl">Recent Club Activities...</div>
-			<Link to='newpost' className="text-2xl">Add new post</Link>
+			<Link to='newpost'><div className="text-2xl text-center mt-5 text-green-500">Add new post</div></Link>
 			{!getFeed.loading && feed.map(post => <Post key={post.id} post={post}/>)}
 		</div>
 	)
@@ -23,13 +23,13 @@ function Post(props)
 {
 	const post = props.post;
 	return (
-		<div className="flex flex-col gap-8 border-solid border bg-neutral-100 rounded-md w-2/5 mx-auto my-5 p-10">
+		<div className="flex flex-col gap-8 border-solid border bg-neutral-100 rounded-md w-2/5 mx-auto my-5 p-10 break-words">
 			<div>
 				<Link to={`${post.id}`} className="text-xl font-bold">{post.title}</Link>
 				<div className="text-sm">by {<Link to={`/account/${post.author.id}`}>{post.author.username}</Link>} for {post.club ? <Link to={`/club/${post.club.id}`}>{post.club.name}</Link> : "DELETED CLUB"}</div>
 			</div>
 			<div className="text-xl">{props.post.body}</div>
-			{post.file && <img className="mx-auto" width={'400rem'} src={`/images/${post.file}`}/>}
+			{post.file && <img className="mx-auto" alt="post" width={'400rem'} src={`/images/${post.file}`}/>}
 		</div>
 	)
 }
