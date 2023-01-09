15daa6262ecdc62dbaf0497c2d36b528c209c37c
enyoukai <an.josephZero@gmail.com>
2023-01-08 20:39:25 -0800

    adding profile picture upload

--- frontend/src/routes/Settings.js
+++ frontend/src/routes/Settings.js
@@ -1,7 +1,19 @@
+import DropZone from 'components/DropZone';
+
+import { useState } from 'react';
+
 export default function Settings() {
+	const [profilePicture, setProfilePicture] = useState();
+
+	function onFileUpload(files)
+	{
+		setProfilePicture(files[0]);
+	}
+	
 	return (
 		<div>
 			<h2>Settings</h2>
+			<DropZone currentImg={profilePicture} onDrop={onFileUpload}/>
 		</div>
 	)
 }
